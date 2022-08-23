1d536ef21c18c662884d40b93629221f02dcf1a4
IsmailAlamir <ismaeelalamir@gmail.com>
2022-08-23 20:54:18 +0300

    pass function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import data from "./components/data";
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import SelectedBeast from './components/SelectedBeast';
 
 
-class App extends React.Component{
+class App extends React.Component {
 
-render (){
-  return (<div>
-
-<Header/>
-<Main/>
-<Footer/>
-
-  </div>)
+constructor(props){
+  super(props);
+  this.state={
+            welcome:"...........",
+            data:data
+  }
 }
 
+welcomeing=()=>{
+  this.setState({
+    welcome:"HELLO!"
+  })
+}
+
+  render() {
+    return (
+
+      <div style={{ width: "100%", textAlign: 'center', margin: "0", padding: "20px" }}>
+        <Header />
+        <h1>{this.state.welcome}</h1>
+        <Main data={data} update={this.welcomeing} />
+        <Footer />
+      </div>)
+  }
+
 
 }
 
-export default App ;
+export default App;
